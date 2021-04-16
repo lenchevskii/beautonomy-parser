4c96635e6b0b8600bce0a4ea1d01fde8a54381a9
dzmitry <dima.lenchevskii@gmail.com>
2021-04-16 15:48:26 +0300

    remove unnecessary, add url

--- broker.js
+++ broker.js
@@ -21,5 +21,5 @@ broker.createService({
 })
 
 broker.start()
-      .then(() => broker.call('fire.downloadImage', { url: "https://scontent-waw1-1.cdninstagram.com/v/t51.2885-15/e35/s1080x1080/170586863_2917158181863045_2391084497817864474_n.jpg?tp=1&_nc_ht=scontent-waw1-1.cdninstagram.com&_nc_cat=1&_nc_ohc=A_cjqR0LRz0AX-Km0cz&edm=AABBvjUAAAAA&ccb=7-4&oh=9414750ba9da5d319af7698db620c186&oe=60A05EEF&_nc_sid=83d603"}))
+      .then(() => broker.call('fire.downloadImage', { url: "https://raw.githubusercontent.com/lenchevskii/beautonomy-parser/main/docs/beautonomy_mining_scheme.jpg"}))
       // .then(res => FS.writeFile('tmp/nasa.jpg', res))
--- puppeteer-firefox.js
+++
@@ -1,6 +0,0 @@
-const puppeteer = require('puppeteer')
-
-const browser = puppeteer.launch({ product: 'firefox', headless: false })
-
-// browser.newPage('http://google.com')
-module.exports = { browser }
